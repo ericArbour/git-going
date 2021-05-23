d195fac54d59d6430cfcef79e3553e8c5ef96a26
Eric Arbour <arboureric@gmail.com>
2021-05-23 13:12:45 -0400

    Improve controller logging

--- client/controllers/branch-controller.ts
+++ client/controllers/branch-controller.ts
@@ -8,22 +8,27 @@ export class BranchController extends Controller {
   static values = { name: String };
 
   readonly nameValue?: string;
-
   eventSource?: EventSource;
+  basePath = '/branch/sse/' as const;
+  path?: string;
 
   connect() {
     if (typeof this.nameValue === 'string') {
-      const path = `/branch/sse/${this.nameValue}`;
-      console.log(`connecting to stream source ${path}`);
-      this.eventSource = new EventSource(path);
+      this.path = `${this.basePath}${this.nameValue}`;
+      console.log(`connecting to stream source ${this.path}`);
+      this.eventSource = new EventSource(this.path);
       connectStreamSource(this.eventSource);
     } else {
-      console.error('Cannot connect to stream source: invalid branch name');
+      console.error(
+        'Cannot connect to stream source - invalid branch name:',
+        this.nameValue,
+      );
     }
   }
 
   disconnect() {
     if (this.eventSource) {
+      console.log(`disconnecting from stream source ${this.path}`);
       disconnectStreamSource(this.eventSource);
       this.eventSource.close();
     }
--- client/controllers/branches-controller.ts
+++ client/controllers/branches-controller.ts
@@ -6,14 +6,17 @@ import {
 
 export class BranchesController extends Controller {
   eventSource?: EventSource;
+  path = '/branches/sse' as const;
 
   connect() {
-    this.eventSource = new EventSource('/branches/sse');
+    console.log(`connecting to stream source ${this.path}`);
+    this.eventSource = new EventSource(this.path);
     connectStreamSource(this.eventSource);
   }
 
   disconnect() {
     if (this.eventSource) {
+      console.log(`disconnecting from stream source ${this.path}`);
       disconnectStreamSource(this.eventSource);
       this.eventSource.close();
     }
